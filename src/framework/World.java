ef672b42d410605036a562d0510e741622f1421a
dforero <d.enrique.forero@gmail.com>
2015-01-24 02:15:11 -0800

    Updating World.java

--- src/framework/World.java
+++ src/framework/World.java
@@ -1,6 +1,26 @@
 package framework;
 
 public class World {
-	//collection of tiles
-
+	private int playerNum;
+	private int maxPlayerNum;
+	
+	public World() {
+		playerNum = 0;
+		maxPlayerNum = 2;
+	}
+	
+	public int AddPlayer() {
+		if (playerNum >= maxPlayerNum) {
+			return -1;
+		}
+		return playerNum++;
+	}
+	
+	public int GetPlayerNum() {
+		return playerNum;
+	}
+	
+	public int GetMaxPlayerNum() {
+		return maxPlayerNum;
+	}
 }
--- src/server/temp2.java
+++
@@ -1,5 +0,0 @@
-package server;
-
-public class temp2 {
-
-}
